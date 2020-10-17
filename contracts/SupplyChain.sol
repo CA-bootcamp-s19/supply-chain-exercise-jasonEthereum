6095dc457dba919306370c683d258c5396ce0fb1
jasonEthereum <70664782+jasonEthereum@users.noreply.github.com>
2020-10-17 13:17:15 +0100

    Update SupplyChain.sol

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,115 +1,138 @@
-/*
-    This exercise has been updated to use Solidity version 0.6
+    /*
+    This exercise has been updated to use Solidity version 0.6.12
     Breaking changes from 0.5 to 0.6 can be found here: 
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
 
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity ^0.6.12;
 
-contract SupplyChain {
+contract SimpleBank {
 
-  /* set owner */
-  address owner;
+    //
+    // State variables
+    //
 
-  /* Add a variable called skuCount to track the most recent sku # */
+    /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
+  //  mapping (address => uint) balances;
+    mapping(address => uint) balances;    
 
-  /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
-     Call this mappings items
-  */
+    /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
+    mapping (address => bool) public enrolled;
 
-  /* Add a line that creates an enum called State. This should have 4 states
-    ForSale
-    Sold
-    Shipped
-    Received
-    (declaring them in this order is important for testing)
-  */
+    /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
+    address owner;
 
-  /* Create a struct named Item.
-    Here, add a name, sku, price, state, seller, and buyer
-    We've left you to figure out what the appropriate types are,
-    if you need help you can ask around :)
-    Be sure to add "payable" to addresses that will be handling value transfer
-  */
+    //
+    // Events - publicize actions to external listeners
+    // ( I added dates to these events)
+    
+    /* Add an argument for this event, an accountAddress */
+    event LogEnrolled(
+//        uint256 date,
+        address accountAddress
+        );
 
-  /* Create 4 events with the same name as each possible State (see above)
-    Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
-    Each event should accept one argument, the sku */
+    /* Add 2 arguments for this event, an accountAddress and an amount */
+    event LogDepositMade(
+  //      uint256 date,
+        address accountAddress, 
+        uint amount
+        );
 
-/* Create a modifer that checks if the msg.sender is the owner of the contract */
+    /* Create an event called LogWithdrawal */
+    /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
 
-  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
-
-  modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
-  modifier checkValue(uint _sku) {
-    //refund them after pay for item (why it is before, _ checks for logic before func)
-    _;
-    uint _price = items[_sku].price;
-    uint amountToRefund = msg.value - _price;
-    items[_sku].buyer.transfer(amountToRefund);
-  }
-
-  /* For each of the following modifiers, use what you learned about modifiers
-   to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. 
-   Note that the uninitialized Item.State is 0, which is also the index of the ForSale value,
-   so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
-   Hint: What item properties will be non-zero when an Item has been added?
-   
-   PS: Uncomment the modifier but keep the name for testing purposes!
-   */
-  
-  
-  /// modifier forSale
-  /// modifier sold
-  /// modifier shipped
-  /// modifier received
+    event LogWithdrawal(
+        address accountAddress, 
+        uint withdrawAmount,
+        uint newBalance
+        );
 
 
-  constructor() public {
-    /* Here, set the owner as the person who instantiated the contract
-       and set your skuCount to 0. */
-  }
+    //
+    // Functions
+    //
 
-  function addItem(string memory _name, uint _price) public returns(bool){
-    emit LogForSale(skuCount);
-    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
-    skuCount = skuCount + 1;
-    return true;
-  }
+    /* Use the appropriate global variable to get the sender of the transaction */
+    constructor() public {
+        /* Set the owner to the creator of this contract */
+        owner = address(this);
+    }
 
-  /* Add a keyword so the function can be paid. This function should transfer money
-    to the seller, set the buyer as the person who called this transaction, and set the state
-    to Sold. Be careful, this function should use 3 modifiers to check if the item is for sale,
-    if the buyer paid enough, and check the value after the function is called to make sure the buyer is
-    refunded any excess ether sent. Remember to call the event associated with this function!*/
+    // Fallback function - Called if other functions don't match call or
+    // sent ether without data
+    // Typically, called when invalid data is sent
+    // Added so ether sent to this contract is reverted if the contract fails
+    // otherwise, the sender's money is transferred to contract
+    
+    fallback() external payable {
+        revert();
+    }
 
-  function buyItem(uint sku)
-    public
-  {}
+    /// @notice Get balance
+    /// @return The balance of the user
+    // A SPECIAL KEYWORD prevents function from editing state variables;
+    // allows function to run locally/off blockchain
+    function getBalance() public view returns (uint) {
+        /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
+    }
+    function getEnrolledStatus() public view returns (bool) {
+        /* Get the balance of the sender of this transaction */
+        return enrolled[msg.sender];
+    }
 
-  /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
-  is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
-  function shipItem(uint sku)
-    public
-  {}
 
-  /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
-  is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
-  function receiveItem(uint sku)
-    public
-  {}
 
-  /* We have these functions completed so we can run tests, just ignore it :) */
-  /*
-  function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
-    name = items[_sku].name;
-    sku = items[_sku].sku;
-    price = items[_sku].price;
-    state = uint(items[_sku].state);
-    seller = items[_sku].seller;
-    buyer = items[_sku].buyer;
-    return (name, sku, price, state, seller, buyer);
-  } */
+    /// @notice Enroll a customer with the bank
+    /// @return The users enrolled status
+    // Emit the appropriate event
+    function enroll() public returns (bool){
+        enrolled[msg.sender] = true; 
+        emit LogEnrolled( msg.sender);
+        return enrolled[msg.sender];
+    }
 
+    /// @notice Deposit ether into bank
+    /// @return The balance of the user after the deposit is made
+    // Add the appropriate keyword so that this function can receive ether
+    // Use the appropriate global variables to get the transaction sender and value
+    // Emit the appropriate event    
+    // Users should be enrolled before they can make deposits
+    
+    
+    
+    function deposit(uint _depositAmt) public returns (uint) {
+        /* Add the amount to the user's balance, call the event associated with a deposit,
+          then return the balance of the user */
+        if (enrolled[msg.sender] == true) {
+        balances[msg.sender] += _depositAmt;
+        emit LogDepositMade( msg.sender, _depositAmt);
+        }
+        return(balances[msg.sender]);
+    }
+    
+
+
+    // @notice Withdraw ether from bank
+    // @dev This does not return any excess ether sent to it
+    // @param withdrawAmount amount you want to withdraw
+    // @return The balance remaining for the user
+    // Emit the appropriate event    
+    function withdraw(uint _withdrawAmount) public returns (uint) {
+        /* If the sender's balance is at least the amount they want to withdraw,
+           Subtract the amount from the sender's balance, and try to send that amount of ether
+           to the user attempting to withdraw. 
+           return the user's balance.*/
+        require (enrolled[msg.sender] == true);
+
+        // Check withdrawl amount 
+        require(_withdrawAmount <= balances[msg.sender]);
+
+        balances[msg.sender] -= _withdrawAmount;
+        emit LogWithdrawal( msg.sender, _withdrawAmount, balances[msg.sender]);
+        return(balances[msg.sender]);
+    }
+
+    
 }
